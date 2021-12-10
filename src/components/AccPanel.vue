2161c845ecea9a7863aa20f30cd808461dad89a9
Zh <zeming@dreamofsky.com>
2021-12-10 11:37:54 -0500

    added fields to acc page

--- src/components/AccPanel.vue
+++ src/components/AccPanel.vue
@@ -3,6 +3,12 @@
     <h2>Account Info</h2>
     <p>
       <label>Get Accounts</label>
+      <label>fields</label>
+      <select v-model="fields" multiple size="2">
+        <option v-for="option in fieldsOptions" :key="option">
+          {{ option }}
+        </option>
+      </select>
       <button value="get" @click="getAccs">get</button>
     </p>
     <p>{{ screenStr }}</p>
@@ -15,15 +21,19 @@ export default {
   props: ['APIClient', 'isLogIn'],
   data: () => ({
     screenStr: null,
+    fields: null,
+    fieldsOptions: ['orders', 'positions'],
   }),
   methods: {
     async getAcc() {
-      const acc = await this.APIClient.accounts.getAccount();
+      const { fields } = this;
+      const acc = await this.APIClient.accounts.getAccount({ fields });
       console.log(acc);
       this.screenStr = JSON.stringify(acc);
     },
     async getAccs() {
-      const accs = await this.APIClient.accounts.getAccounts();
+      const { fields } = this;
+      const accs = await this.APIClient.accounts.getAccounts({ fields });
       console.log(accs);
       this.screenStr = JSON.stringify(accs);
     },
